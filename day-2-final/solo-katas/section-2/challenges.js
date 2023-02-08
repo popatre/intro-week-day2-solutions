96a67395dcddce4a7b0f625082d7655b0c620323
popatre <76402245+popatre@users.noreply.github.com>
2023-02-08 10:58:35 +0000

    add solutions -solo-section2

--- day-2-final/solo-katas/section-2/challenges.js
+++ day-2-final/solo-katas/section-2/challenges.js
@@ -12,9 +12,10 @@ When you're ready to move on to the next function replace skipTest with runTest.
 // Exercise 1
 function checkIfPropertyExists(obj, key) {
   // This function should take an object and a key as its arguments and return true if the input object contains the provided key and false otherwise
+  return Boolean(obj[key]);
 }
 
-skipTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
+runTest("checkIfPropertyExists() checks if a property exists inside an object", function () {
   check(checkIfPropertyExists({ name: "jonny", age: 32 }, "name")).isEqualTo(true);
   check(checkIfPropertyExists({ name: "jonny", age: 32 }, "age")).isEqualTo(true);
   check(checkIfPropertyExists({ name: "jonny", age: 32 }, "pets")).isEqualTo(false);
@@ -23,9 +24,10 @@ skipTest("checkIfPropertyExists() checks if a property exists inside an object",
 // Exercise 2
 function getLastItem(arr) {
   //Write your function here...
+  return arr.pop();
 }
 
-skipTest("getLastItem() returns the last item in an array", function () {
+runTest("getLastItem() returns the last item in an array", function () {
   check(getLastItem(["a", "b", "c", "d"])).isEqualTo("d");
   check(getLastItem(["apple", "banana", "pear", "kiwi"])).isEqualTo("kiwi");
 });
@@ -33,9 +35,12 @@ skipTest("getLastItem() returns the last item in an array", function () {
 // Exercise 3
 function createObject(arr) {
   // This function should take an array consisting of two elements representing a key/ value pair as its argument and return an object with a single property based on the input
+  const result = {};
+  result[arr[0]] = arr[1];
+  return result;
 }
 
-skipTest("createObject() creates a new object from a key value pair", function () {
+runTest("createObject() creates a new object from a key value pair", function () {
   check(createObject(["name", "shaq"])).isEqualTo({ name: "shaq" });
   check(createObject(["fruit", "apple"])).isEqualTo({ fruit: "apple" });
   check(createObject(["language", "haskell"])).isEqualTo({ language: "haskell" });
@@ -44,9 +49,11 @@ skipTest("createObject() creates a new object from a key value pair", function (
 // Exercise 4
 function getLastNItems(arr, n) {
   // This function should take two arguments, an array and a number n, and return a new array containing the last n items of the given array
+  if (n === 0) return [];
+  return arr.slice(-n);
 }
 
-skipTest("getLastNItems() returns the last n items in an array", function () {
+runTest("getLastNItems() returns the last n items in an array", function () {
   check(getLastNItems(["a", "b", "c", "d"], 2)).isEqualTo(["c", "d"]);
   check(getLastNItems(["apple", "banana", "pear", "kiwi"], 0)).isEqualTo([]);
   check(getLastNItems(["apple", "banana", "pear", "kiwi"], 3)).isEqualTo(["banana", "pear", "kiwi"]);
@@ -56,9 +63,16 @@ skipTest("getLastNItems() returns the last n items in an array", function () {
 function createArrow(direction) {
   // This function should take a string representing a direction ("left", "right", "up" or "down") as its argument and return the corresponding arrow ("←", "→", "↑", "↓")
   // You don't need to utilise an object here, but think about how you could do so
+  const arrows = {
+    right: "→",
+    left: "←",
+    up: "↑",
+    down: "↓",
+  };
+  return arrows[direction];
 }
 
-skipTest("createArrow() will return an arrow pointing in the right direction", function () {
+runTest("createArrow() will return an arrow pointing in the right direction", function () {
   check(createArrow("left")).isEqualTo("←");
   check(createArrow("right")).isEqualTo("→");
   check(createArrow("up")).isEqualTo("↑");
@@ -68,9 +82,11 @@ skipTest("createArrow() will return an arrow pointing in the right direction", f
 // Exercise 6
 function removeItem(arr, n) {
   // This function should take two arguments, an array and a number n, and return a new array without the item at index 'n'
+  arr.splice(n, 1);
+  return arr;
 }
 
-skipTest("removeItem() removes an item at a given index", function () {
+runTest("removeItem() removes an item at a given index", function () {
   check(removeItem(["a", "b", "c", "d"], 2)).isEqualTo(["a", "b", "d"]);
   check(removeItem(["a", "b", "c", "d"], 0)).isEqualTo(["b", "c", "d"]);
   check(removeItem(["a", "b", "c", "d"], 1)).isEqualTo(["a", "c", "d"]);
@@ -93,9 +109,10 @@ function updateVoterAddress(voter, correctHouseNumber) {
   The voter's house number is incorrect and you should update it to be the one provided
   This function does NOT need to return anything
   */
+  voter.address.houseNumber = correctHouseNumber;
 }
 
-skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
+runTest("updateVoterAddress() updates the voter's houseNumber", function () {
   const voter = {
     name: "Alex",
     age: 39,
@@ -122,9 +139,10 @@ skipTest("updateVoterAddress() updates the voter's houseNumber", function () {
 //Exercise 7
 function mergeArrays(arr1, arr2) {
   // This function should take two arrays as arguments and return a new array containing all of arr1 and arr2's elements (in that order)
+  return arr1.concat(arr2);
 }
 
-skipTest("mergeArrays() will concatenate two arrays together", function () {
+runTest("mergeArrays() will concatenate two arrays together", function () {
   check(mergeArrays(["a", "b"], ["c", "d"])).isEqualTo(["a", "b", "c", "d"]);
   check(mergeArrays([1], [3, 5, 7])).isEqualTo([1, 3, 5, 7]);
   check(mergeArrays(["x", "y", "z"], [1, 2, 3, 4])).isEqualTo(["x", "y", "z", 1, 2, 3, 4]);
