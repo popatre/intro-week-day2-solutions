bc4b7742b0bea3be60142da2ee94b252dee0dead
popatre <76402245+popatre@users.noreply.github.com>
2023-02-08 10:51:42 +0000

    add solutions solo-section1

--- day-2-final/solo-katas/section-1/exercises.js
+++ day-2-final/solo-katas/section-1/exercises.js
@@ -12,60 +12,60 @@ e.g.  check(FILL_ME_IN).isEqualTo(2 + 2) becomes -> check(4).isEqualTo(2 + 2)
 runTest("checking multiTypeArray", function () {
   const multiTypeArray = ["I am a string", 42, true, [1, 2, 3]];
 
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[0]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[1]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[2]);
-  check(FILL_ME_IN).isEqualTo(typeof multiTypeArray[3]);
+  check("string").isEqualTo(typeof multiTypeArray[0]);
+  check("number").isEqualTo(typeof multiTypeArray[1]);
+  check("boolean").isEqualTo(typeof multiTypeArray[2]);
+  check("object").isEqualTo(typeof multiTypeArray[3]);
 });
 
 // Exercise 2
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("checking alphaSample", function () {
+runTest("checking alphaSample", function () {
   const alphaSample = ["a", "b", "c"];
   alphaSample.push("d");
   alphaSample.push("g");
 
-  check(FILL_ME_IN).isEqualTo(alphaSample);
+  check(["a", "b", "c", "d", "g"]).isEqualTo(alphaSample);
 
   const lastItem = alphaSample.pop();
 
-  check(FILL_ME_IN).isEqualTo(lastItem);
-  check(FILL_ME_IN).isEqualTo(alphaSample);
+  check("g").isEqualTo(lastItem);
+  check(["a", "b", "c", "d"]).isEqualTo(alphaSample);
 });
 
 // Exercise 3
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("working with nested arrays", function () {
+runTest("working with nested arrays", function () {
   const rows = [
     ["a", "b", "c"],
     ["d", "e", "f"],
     ["g", "h", "i"],
   ];
 
-  check(FILL_ME_IN).isEqualTo(rows[0]);
-  check(FILL_ME_IN).isEqualTo(rows[1]);
-  check(FILL_ME_IN).isEqualTo(rows[2]);
+  check(["a", "b", "c"]).isEqualTo(rows[0]);
+  check(["d", "e", "f"]).isEqualTo(rows[1]);
+  check(["g", "h", "i"]).isEqualTo(rows[2]);
 
   const firstRow = rows[0];
   check(firstRow[0]).isEqualTo("a");
-  check(firstRow[FILL_ME_IN]).isEqualTo("b");
+  check(firstRow[1]).isEqualTo("b");
 
-  check(rows[1][FILL_ME_IN]).isEqualTo("e");
-  check(rows[2][FILL_ME_IN]).isEqualTo("g");
-  check(rows[0][FILL_ME_IN]).isEqualTo("c");
+  check(rows[1][1]).isEqualTo("e");
+  check(rows[2][0]).isEqualTo("g");
+  check(rows[0][2]).isEqualTo("c");
 });
 
 // Exercise 4
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("get the slice of an array", function () {
+runTest("get the slice of an array", function () {
   const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
 
-  check(FILL_ME_IN).isEqualTo(letters.slice(1, 4));
-  check(FILL_ME_IN).isEqualTo(letters.slice(2, 3));
-  check(FILL_ME_IN).isEqualTo(letters.slice(3, 5));
-  check(FILL_ME_IN).isEqualTo(letters.slice(-1));
-  check(FILL_ME_IN).isEqualTo(letters.slice(-2));
-  check(FILL_ME_IN).isEqualTo(letters.slice(0, -4));
+  check(["b", "c", "d"]).isEqualTo(letters.slice(1, 4));
+  check(["c"]).isEqualTo(letters.slice(2, 3));
+  check(["d", "e"]).isEqualTo(letters.slice(3, 5));
+  check(["n"]).isEqualTo(letters.slice(-1));
+  check(["m", "n"]).isEqualTo(letters.slice(-2));
+  check(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]).isEqualTo(letters.slice(0, -4));
 });
 
 // Exercise 5
@@ -73,44 +73,44 @@ skipTest("get the slice of an array", function () {
 runTest("Task 5", function () {
   const myObject = {};
 
-  check(FILL_ME_IN).isEqualTo(typeof myObject);
+  check("object").isEqualTo(typeof myObject);
 });
 
 // Exercise 6
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 6", function () {
+runTest("Task 6", function () {
   const father = {
     firstName: "Michael",
     lastName: "Bluth",
     age: 33,
   };
 
-  check(FILL_ME_IN).isEqualTo(father.firstName);
-  check(FILL_ME_IN).isEqualTo(father.lastName);
-  check(FILL_ME_IN).isEqualTo(father.firstname);
-  check(FILL_ME_IN).isEqualTo(father.age);
+  check("Michael").isEqualTo(father.firstName);
+  check("Bluth").isEqualTo(father.lastName);
+  check(undefined).isEqualTo(father.firstname);
+  check(33).isEqualTo(father.age);
 });
 
 // Exercise 7
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 7", function () {
+runTest("Task 7", function () {
   const brotherInLaw = {
     name: "Tobias",
     lastname: "Funke",
     job: "therapist",
   };
 
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check("therapist").isEqualTo(brotherInLaw.job);
   brotherInLaw.job = "actor";
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check("actor").isEqualTo(brotherInLaw.job);
 
   delete brotherInLaw.job;
-  check(FILL_ME_IN).isEqualTo(brotherInLaw.job);
+  check(undefined).isEqualTo(brotherInLaw.job);
 });
 
 // Exercise 8
 // once you have passed the test above, change skipTest below to runTest so you are able to run the next test
-skipTest("Task 8", function () {
+runTest("Task 8", function () {
   const bluthFamily = {
     father: {
       name: "George",
@@ -122,9 +122,9 @@ skipTest("Task 8", function () {
     daughters: [{ name: "Lindsay" }],
   };
 
-  check(FILL_ME_IN).isEqualTo(bluthFamily.father.name);
-  check(bluthFamily.mother.name).isEqualTo(FILL_ME_IN);
-  check(bluthFamily.daughters[FILL_ME_IN].FILL_ME_IN).isEqualTo("Lindsay");
+  check("George").isEqualTo(bluthFamily.father.name);
+  check(bluthFamily.mother.name).isEqualTo("Lucille");
+  check(bluthFamily.daughters[0].name).isEqualTo("Lindsay");
 });
 
 // >>>>>>>>>>> DON'T ALTER ANYTHING BELOW THIS LINE <<<<<<<<<<<<<<<
