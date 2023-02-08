33ad93bbb782c2f79f458719011f66dbcd96aa47
popatre <76402245+popatre@users.noreply.github.com>
2023-02-08 11:37:05 +0000

    add solutions paired:section3

--- day-2-final/paired-katas/section-3/functions.js
+++ day-2-final/paired-katas/section-3/functions.js
@@ -9,23 +9,20 @@ Take a look at the tests to see what each function should be returning given var
 When you're ready to move on to the next function replace skipTest with runTest.
 */
 //Question 0
-/*
-function double() 
-  This function should return any number doubled
-*/
+function double(num) {
+  return num * 2;
+}
 
-skipTest("double() can double any number", function () {
+runTest("double() can double any number", function () {
   check(double(11)).isEqualTo(22);
   check(double(129)).isEqualTo(258);
   check(double(25)).isEqualTo(50);
 });
 
 //Question 1
-/*
-function multiply() 
- This function should return the product of two passed numbers
-
-*/
+function multiply(num1, num2) {
+  return num1 * num2;
+}
 
 runTest("multiply() can multiply 2 numbers together", function () {
   check(multiply(10, 42)).isEqualTo(420);
@@ -34,52 +31,45 @@ runTest("multiply() can multiply 2 numbers together", function () {
 });
 
 // Question 2
-/*
-squareRoot()
+function squareRoot(n) {
+  return Math.sqrt(n);
+}
 
-This function should take a single argument and return its square root
-*/
-
-skipTest("squareRoot() returns the square root of a positive integer", function () {
+runTest("squareRoot() returns the square root of a positive integer", function () {
   check(squareRoot(100)).isEqualTo(10);
   check(squareRoot(25)).isEqualTo(5);
   check(squareRoot(121)).isEqualTo(11);
 });
 
 // Question 3
-/*
-raiseToPower()
+function raiseToPower(m, n) {
+  return m ** n;
+}
 
-This function should take two arguments, m and n, and return m raised to the power of n
-*/
-
-skipTest("raiseToPower() raises given number to the given power", function () {
+runTest("raiseToPower() raises given number to the given power", function () {
   check(raiseToPower(10, 3)).isEqualTo(1000);
   check(raiseToPower(25, 2)).isEqualTo(625);
   check(raiseToPower(10, 0)).isEqualTo(1);
 });
 
 // Question 4
-/*
-capitaliseFirstLetter()
+function capitaliseFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
-This function should take a string as an argument and return the same string with the first letter capitalised
-*/
-skipTest("capitaliseFirstLetter() capitalises the first letter in a string", function () {
+runTest("capitaliseFirstLetter() capitalises the first letter in a string", function () {
   check(capitaliseFirstLetter("bang")).isEqualTo("Bang");
   check(capitaliseFirstLetter("apple")).isEqualTo("Apple");
   check(capitaliseFirstLetter("coding")).isEqualTo("Coding");
 });
 
 // Question 5
-/*
-isFromThe60s()
+function isFromThe60s(num) {
+  // this is the refactor extra challenge solution (without if statements)
+  return num >= 1960 && num <= 1969;
+}
 
-This function should take a number as an argument representing a year, and return true if that year is in the 1960s and false otherwise
-
-*/
-
-skipTest("isFromThe60s() checks if a number is within 1960 to 1969 (inclusive)", function () {
+runTest("isFromThe60s() checks if a number is within 1960 to 1969 (inclusive)", function () {
   check(isFromThe60s(1962)).isEqualTo(true);
   check(isFromThe60s(1965)).isEqualTo(true);
   check(isFromThe60s(1970)).isEqualTo(false);
@@ -90,15 +80,12 @@ skipTest("isFromThe60s() checks if a number is within 1960 to 1969 (inclusive)",
 });
 
 // Question 6
-/*
-isAbsolutePath()
 
-This function should take a string as an argument representing a file path and return true if it is an absolute path, and false otherwise
+function isAbsolutePath(str) {
+  return str.charAt(0) === "/";
+}
 
-HINT: all absolute file paths start with a /
-*/
-
-skipTest("isAbsolutePath() checks if a file path is absolute or relative", function () {
+runTest("isAbsolutePath() checks if a file path is absolute or relative", function () {
   check(isAbsolutePath("/Users/mitch")).isEqualTo(true);
   check(isAbsolutePath("/Users/mitch/northcoders/remote_course/remote_precourse_1")).isEqualTo(true);
   check(isAbsolutePath("../composers")).isEqualTo(false);
@@ -106,35 +93,29 @@ skipTest("isAbsolutePath() checks if a file path is absolute or relative", funct
 });
 
 // Question 7
-/*
-getMiddle()
+function getMiddle(str) {
+  const half = Math.floor(str.length / 2);
+  if (str.length % 2 !== 0) return str.charAt(half);
+  return str.slice(half - 1, half + 1);
+}
 
-This function should take a string as an argument and return the middle (or middle two) character(s) of that string
-
-*/
-
-skipTest("getMiddle() returns the middle character in a string of odd length", function () {
+runTest("getMiddle() returns the middle character in a string of odd length", function () {
   check(getMiddle("abc")).isEqualTo("b");
   check(getMiddle("mitch")).isEqualTo("t");
 });
 
-skipTest("getMiddle() returns the middle characters in a string of even length", function () {
+runTest("getMiddle() returns the middle characters in a string of even length", function () {
   check(getMiddle("abcd")).isEqualTo("bc");
   check(getMiddle("blob")).isEqualTo("lo");
 });
 
 // Question 8
-/*
-getCharCode()
+function getCharCode(letter) {
+  const charCode = letter.charCodeAt(0);
+  return `The ASCII code for ${letter} is ${charCode}`;
+}
 
-This function should take a string as an argument and return a string which describes the ASCII code of that character
-
-The returned string should be in the following format:
-
-"The ASCII code for <character> is <character-code>"
-*/
-
-skipTest("getCharCode() will return a message stating the ASCII code of a passed char", function () {
+runTest("getCharCode() will return a message stating the ASCII code of a passed char", function () {
   check(getCharCode("A")).isEqualTo("The ASCII code for A is 65");
   check(getCharCode("b")).isEqualTo("The ASCII code for b is 98");
   check(getCharCode("z")).isEqualTo("The ASCII code for z is 122");
