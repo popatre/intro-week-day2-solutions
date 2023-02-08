f0bbc1d01fa79c7b80f1e112c8c405b79ad94ff3
popatre <76402245+popatre@users.noreply.github.com>
2023-02-08 11:19:33 +0000

    add solutions paired:section-1

--- day-2-final/paired-katas/section-1/exercises.js
+++ day-2-final/paired-katas/section-1/exercises.js
@@ -1,4 +1,4 @@
-const { check, skipTest, skipTest } = require("../../test-api");
+const { check, skipTest, runTest } = require("../../test-api");
 
 /*
 Instructions
@@ -11,9 +11,10 @@ When you're ready to move on to the next function replace skipTest with skipTest
 //Exercise 1
 function countProperties(obj) {
   // This function should take an object as its argument and return the total number of properties it contains
+  return Object.keys(obj).length;
 }
 
-skipTest("countProperties() counts the number of key-value pairs for a given object", function () {
+runTest("countProperties() counts the number of key-value pairs for a given object", function () {
   check(countProperties({})).isEqualTo(0);
   check(countProperties({ name: "shaq" })).isEqualTo(1);
   check(countProperties({ name: "shaq", job: "tutor", city: "Manchester" })).isEqualTo(3);
@@ -22,9 +23,11 @@ skipTest("countProperties() counts the number of key-value pairs for a given obj
 //Exercise 2
 function isEmptyArray(arr) {
   // This function should take an array as an argument and return true if the array is empty, and false otherwise
+
+  return arr.length === 0;
 }
 
-skipTest("isEmptyArray() checks if an array is empty", function () {
+runTest("isEmptyArray() checks if an array is empty", function () {
   check(isEmptyArray([])).isEqualTo(true);
   check(isEmptyArray(["a", "b", "c", "d"])).isEqualTo(false);
   check(isEmptyArray(["a"])).isEqualTo(false);
@@ -45,9 +48,11 @@ function createUserString(user) {
   This is a good use case for String Template Literals:
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
   */
+
+  return `name: ${user.name}, age: ${user.age}, language: ${user.language}`;
 }
 
-skipTest("createUserString() will create a message from an details object", function () {
+runTest("createUserString() will create a message from an details object", function () {
   check(createUserString({ name: "Mitch", age: 30, language: "Javascript" })).isEqualTo(
     "name: Mitch, age: 30, language: Javascript"
   );
@@ -55,12 +60,14 @@ skipTest("createUserString() will create a message from an details object", func
 });
 
 //Exercise 4
-function howManyArguments() {
+function howManyArguments(...args) {
   // This function should take any number of arguments and return the number of arguments passed into the function
   // HINT: For this one you should look up 'rest parameters' online - MDN Web Docs and devdocs are excellent sources of JavaScript documentation
+
+  return args.length;
 }
 
-skipTest("howManyArguments() returns the number of items passed on a single call", function () {
+runTest("howManyArguments() returns the number of items passed on a single call", function () {
   check(howManyArguments("a", "b", "c")).isEqualTo(3);
   check(howManyArguments()).isEqualTo(0);
   check(howManyArguments(1, 2, 3, 4, 5)).isEqualTo(5);
@@ -80,9 +87,12 @@ function updateCoinMachine(coinMachine, coin) {
   }
   You should 'add the provided coin to the machine' by altering the associated property and return the updated coinMachine
   */
+
+  coinMachine[coin]++;
+  return coinMachine;
 }
 
-skipTest("updateCoinMachine() will create a message from an details object", function () {
+runTest("updateCoinMachine() will create a message from an details object", function () {
   check(updateCoinMachine({ "1p": 0, "2p": 0, "5p": 0, "10p": 0 }, "1p")).isEqualTo({
     "1p": 1,
     "2p": 0,
@@ -123,9 +133,15 @@ function updatePosition(coords, direction) {
   If the direction is "right" it should move 1 unit right (+ 1 in the x direction)
   If the direction is "left" it should move 1 unit left (- 1 in the x direction)
   */
+
+  if (direction === "up") coords[1]++;
+  if (direction === "down") coords[1]--;
+  if (direction === "left") coords[0]--;
+  if (direction === "right") coords[0]++;
+  return coords;
 }
 
-skipTest("updatePosition() updates a co-ordinates array", function () {
+runTest("updatePosition() updates a co-ordinates array", function () {
   check(updatePosition([10, 10], "up")).isEqualTo([10, 11]);
   check(updatePosition([0, 0], "down")).isEqualTo([0, -1]);
   check(updatePosition([3, 3], "left")).isEqualTo([2, 3]);
@@ -141,9 +157,15 @@ function addCoins(collection, coin) {
 
   This should take two arguments, a coin collection array and a string representing a coin, and return an updated version of the given array with the coin added at the appropriate position
   */
+
+  if (coin === "1p") collection[0].push(coin);
+  if (coin === "2p") collection[1].push(coin);
+  if (coin === "5p") collection[2].push(coin);
+  if (coin === "10p") collection[3].push(coin);
+  return collection;
 }
 
-skipTest("addCoins() will update the coins in a given slot", function () {
+runTest("addCoins() will update the coins in a given slot", function () {
   check(addCoins([[], [], [], []], "1p")).isEqualTo([["1p"], [], [], []]);
   check(addCoins([[], [], [], []], "2p")).isEqualTo([[], ["2p"], [], []]);
   check(addCoins([[], ["2p"], [], []], "2p")).isEqualTo([[], ["2p", "2p"], [], []]);
